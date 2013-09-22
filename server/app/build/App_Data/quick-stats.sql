c663f7a36dce37d6234f0f8dd16c961f6e44a9de
Homam <bluesnowball@gmail.com>
2013-09-22 10:15:22 +0400

    build with index fixed

--- server/app/build/App_Data/quick-stats.sql
+++ server/app/build/App_Data/quick-stats.sql
@@ -4,8 +4,8 @@ DECLARE @from DATETIME = '{from}';
 DECLARE @to DATETIME = '{to}';
 
 SELECT
-	(SELECT COUNT (*) FROM dbo.Wap_Visits V  WHERE V.Date_Created > @from AND V.Date_Created < @to AND v.Redirection = 3) AS Visits,
-	(SELECT COUNT(DISTINCT UserId) FROM dbo.MobiSpy_Events WHERE appId = 7 AND ip <> '80.227.47.62' AND creationTime > @from AND creationTime < @to) AS Users,
-	(SELECT COUNT(DISTINCT UserId) FROM dbo.ML_App_User WHERE appId = 7 AND creationTime > @from AND CreationTime < @to) AS RegisteredUsers,
-	(SELECT COUNT(DISTINCT UserId) FROM dbo.ML_App_User WHERE appId = 7 AND creationTime > @from AND CreationTime < @to AND DataSource = 'facebok') AS FacebookConnects,
-	(SELECT COUNT(DISTINCT userId) FROM dbo.MobiSpy_Events E WHERE E.appId = 7 AND ip <> '80.227.47.62' AND E.eventType = 'IAP-Purchased' AND creationTime > @from AND creationTime < @to) AS Purchases
+	(SELECT COUNT (*) FROM dbo.Wap_Visits V WITH(NOLOCK, INDEX(ix_wap_visits))  WHERE V.Date_Created > @from AND V.Date_Created < @to AND v.Redirection = 3) AS Visits,
+	(SELECT COUNT(DISTINCT UserId) FROM dbo.MobiSpy_Events WITH(NOLOCK) WHERE appId = 7 AND ip <> '80.227.47.62' AND creationTime > @from AND creationTime < @to) AS Users,
+	(SELECT COUNT(DISTINCT UserId) FROM dbo.ML_App_User WITH(NOLOCK) WHERE appId = 7 AND creationTime > @from AND CreationTime < @to) AS RegisteredUsers,
+	(SELECT COUNT(DISTINCT UserId) FROM dbo.ML_App_User WITH(NOLOCK) WHERE appId = 7 AND creationTime > @from AND CreationTime < @to AND DataSource = 'facebok') AS FacebookConnects,
+	(SELECT COUNT(DISTINCT userId) FROM dbo.MobiSpy_Events E WITH(NOLOCK) WHERE E.appId = 7 AND ip <> '80.227.47.62' AND E.eventType = 'IAP-Purchased' AND creationTime > @from AND creationTime < @to) AS Purchases
